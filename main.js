ff6adf416d6482abb81ba03da938728961547a40
Hyslach653 <tonies395@gmail.com>
2023-05-05 14:01:34 +0200

    changed roughness

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
-  roughness: 0.2,
+  roughness: 0.5,
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
